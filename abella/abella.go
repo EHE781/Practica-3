82056e766c4083fbcc2dee296e5eaab2e9db3ae3
EHE781 <emanuel.hegedus18@gmail.com>
2022-01-04 17:16:55 +0100

    Finalizado

--- abella/abella.go
+++ abella/abella.go
@@ -11,12 +11,11 @@ import (
 )
 
 const (
-	MAX_TRABAJO = 10
-	colaBote    = "Cola_Bote"
-	colaAbeja   = "Cola_Abeja"
-	colaOso     = "Cola_Oso"
-	finMiel     = "He acabado de poner miel"
-	roto        = "Bote roto"
+	colaAbejas    = "Cola_Abejas"
+	colaAbeja     = "Cola_Abeja_"
+	roto          = "Bote roto"
+	mensajeOso    = "El oso esta comiendo, le ha despertado "
+	colaDespertar = "Despertar"
 )
 
 var (
@@ -32,6 +31,7 @@ func failOnError(err error, msg string) {
 func main() {
 	//Tomar el nombre de la abeja de os.Args[1]
 	if len(os.Args) == 2 {
+		iniciar := false
 		//Mensaje de quien eres
 		fmt.Println("Hola, soy la abeja " + os.Args[1])
 		textoRecibido := ""
@@ -40,62 +40,90 @@ func main() {
 		failOnError(err, "Failed to connect to RabbitMQ")
 		defer conn.Close()
 
-		canalAbeja, err := conn.Channel()
+		canal, err := conn.Channel()
 		failOnError(err, "Failed to open a channel")
-		defer canalAbeja.Close()
+		defer canal.Close()
 
-		canalBote, err := conn.Channel()
-		failOnError(err, "Failed to open channel")
-		defer canalBote.Close()
-
-		colaAbejas, err := canalAbeja.QueueDeclare(
-			colaAbeja, // name
-			false,     // durable
-			false,     // delete when unused
-			false,     // exclusive
-			false,     // no-wait
-			nil,       // arguments
+		colaAbejas, err := canal.QueueDeclare(
+			colaAbejas, // name
+			false,      // durable
+			false,      // delete when unused
+			false,      // exclusive
+			false,      // no-wait
+			nil,        // arguments
 		)
 		failOnError(err, "Failed to declare a queue")
 
-		colaBote, err := canalBote.QueueDeclare(
-			colaBote, // name
-			false,    // durable
-			false,    // delete when unused
-			false,    // exclusive
+		colaPropia, err := canal.QueueDeclare(
+			colaAbeja+os.Args[1], // name
+			false,                // durable
+			false,                // delete when unused
+			false,                // exclusive
+			false,                // no-wait
+			nil,                  // arguments
+		)
+		failOnError(err, "Failed to declare a queue")
+
+		colaDespertar, err := canal.QueueDeclare(
+			colaDespertar, // name
+			false,         // durable
+			false,         // delete when unused
+			false,         // exclusive
+			false,         // no-wait
+			nil,           // arguments
+		)
+		failOnError(err, "Failed to declare queue")
+
+		err = canal.ExchangeDeclare(
+			"fin",    // name
+			"fanout", // type
+			true,     // durable
+			false,    // auto-deleted
+			false,    // internal
 			false,    // no-wait
 			nil,      // arguments
 		)
-		failOnError(err, "Failed to declare a queue")
+		failOnError(err, "Failed to declare queue")
 
-		mensajesBote, err := canalBote.Consume(
-			colaBote.Name, // queue
-			"",            // consumer
-			true,          // auto-ack
-			false,         // exclusive
-			false,         // no-local
-			false,         // no-wait
-			nil,           // args
+		err = canal.QueueBind(
+			colaAbeja+os.Args[1], // queue name
+			"",                   // routing key
+			"fin",                // exchange
+			false,
+			nil,
+		)
+		failOnError(err, "Failed to declare queue")
+
+		mensajesOso, err := canal.Consume(
+			colaPropia.Name, // queue
+			"",              // consumer
+			true,            // auto-ack
+			false,           // exclusive
+			false,           // no-local
+			false,           // no-wait
+			nil,             // args
 		)
 		failOnError(err, "Failed to register a consumer")
 
 		go func() {
-			for mensaje := range mensajesBote {
+			for mensaje := range mensajesOso {
+				textoRecibido = string(mensaje.Body)
 				//El bote envia nuestro nombre y la iteracion, contiene nuestro nombre?
-				if strings.Contains(string((mensaje.Body)), os.Args[1]) {
-					textoRecibido = string(mensaje.Body)
+				if strings.Contains(textoRecibido, os.Args[1]) && iniciar {
 					esperar.Done()
 				}
-				//if string(mensaje.Body) == roto {
-				//poner algo para terminar el proceso entero
-				//}
+				if string(mensaje.Body) == roto {
+					canal.Close()
+					esperar.Done()
+				}
 			}
 		}()
 		//Empezar a llenar de miel 10 veces (spoiler: for con wait (paquete sync))
-		for !canalBote.IsClosed() {
+		for !canal.IsClosed() {
+			iniciar = true
 			esperar.Add(1)
 			log.Println(os.Args[1] + " quiere poner miel...")
-			err = canalAbeja.Publish(
+			err = canal.Publish(
 				"",              // exchange
 				colaAbejas.Name, // routing key
 				false,           // mandatory
@@ -109,8 +137,17 @@ func main() {
 			esperar.Wait()
 			log.Println(os.Args[1] + " pone miel en el bote -> [" + textoRecibido + "]")
 			if strings.Contains(textoRecibido, "10") {
-				//despertar al oso de mierda
-				log.Println("Voy a pinchar al oso...")
+				//despertar al oso
+
+				canal.Publish(
+					"",                 // exchange
+					colaDespertar.Name, // routing key
+					false,              // mandatory
+					false,              // immediate
+					amqp.Publishing{
+						ContentType: "text/plain",
+						Body:        []byte(os.Args[1]),
+					})
 			}
 			//Avisar al bote que ha pasado
 
--- execucio.bat
+++ execucio.bat
@@ -1,13 +1,13 @@
 @echo off
 
-::Willy Jota Zeta Alpha Omega Jane Monsa
+:: Willy Jota Zeta Alpha Omega Jane Monsa
 
-set abelles=Maya Reina Pana 
+set abelles=Maya Reina Pana
 
 ::START/B go run pot/bote.go
 
 FOR %%A IN (%abelles%) DO (
-  START/B go run abella/abella.go %%A
+  START go run abella/abella.go %%A
 )
 
 
